80de3b0dd72e65510ed88886529d4a14c7a9897a
lyh <liu1564684361@163.com>
2025-09-17 11:40:55 +0800

    Second version MaxProfit

--- src/basic/MaxProfit.java
+++ src/basic/MaxProfit.java
@@ -3,12 +3,12 @@ package basic;
 public class MaxProfit {
     public int maxProfit(int[] price){
         int n = price.length;
-        int max = 0;
-        for(int i = 0; i < n - 1; i++){
-            for(int j = i + 1; j < n; j++){
-                if(price[j] - price[i] > max) max = price[j] - price[i];
-            }
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for(int i = 0; i < n; i++){
+            minPrice = Math.min(minPrice, price[i]);
+            maxProfit = Math.max(maxProfit, price[i] - minPrice);
         }
-        return max;
+        return maxProfit;
     }
 }
