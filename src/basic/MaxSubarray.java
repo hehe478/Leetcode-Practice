808404c4d9d4ef270d86f61b5da0e3c277d34e2b
lyh <liu1564684361@163.com>
2025-09-17 15:50:23 +0800

    Divide and conquer approach to solve MaxSubArray

--- src/basic/MaxSubarray.java
+++ src/basic/MaxSubarray.java
@@ -2,13 +2,33 @@ package basic;
 
 public class MaxSubarray {
     public int maxSubArray(int[] nums){
-        int currentSum = 0;
-        int maxSum = Integer.MIN_VALUE;
-        for (int num : nums) {
-            currentSum += num;
-            maxSum = Math.max(maxSum, currentSum);
-            currentSum = Math.max(currentSum, 0);
+        if(nums == null || nums.length == 0) return 0;
+        return findMaxSubArray(nums,0,nums.length - 1);
+    }
+    int findMaxSubArray(int[] nums ,int left ,int right){
+        if(left == right){
+            return(nums[left]);
         }
-        return maxSum;
+        int mid = left + (right - left) / 2;
+        int leftMax = findMaxSubArray(nums,left,mid);
+        int rightMax = findMaxSubArray(nums,mid + 1,right);
+
+        int currentSum = 0;
+        int leftBoardSum = Integer.MIN_VALUE;
+        int rightBoardSum = Integer.MIN_VALUE;
+
+        for(int i = mid; i >= left; i--){
+            currentSum += nums[i];
+            leftBoardSum = Math.max(currentSum,leftBoardSum);
+        }
+        currentSum = 0;
+        for(int i = mid + 1; i <= right; i++){
+            currentSum += nums[i];
+            rightBoardSum = Math.max(currentSum,rightBoardSum);
+        }
+        int mergeSum = leftBoardSum + rightBoardSum;
+        int result = Math.max(leftMax,rightMax);
+        result = Math.max(mergeSum,result);
+        return result;
     }
 }
