35dd0fb2e3f823d2589d057be33c80ba823ed14f
lyh <liu1564684361@163.com>
2025-09-20 17:47:16 +0800

    My learn about LinkedList

--- src/practice/Learn.java
+++ src/practice/Learn.java
@@ -3,7 +3,7 @@ package practice;
 import java.util.ArrayList;
 
 public class Learn {
-    public void learn(){
+    public void learnArray(){
         ArrayList<Integer> arr = new ArrayList<>();
 //        增删查改操作
 
@@ -18,5 +18,29 @@ public class Learn {
         int a = arr.get(1);
 
         arr.set(1,100);
+    }
+
+//    转化数组为链表
+    public <E> LearnListNode<E> createLinkedList(E[] arr){
+        if(arr == null || arr.length == 0) return null;
+        LearnListNode<E> head = new LearnListNode<>(arr[0]);
+        LearnListNode<E> cur = head;
+        for(int i = 1; i < arr.length; i++){
+            cur.next = new LearnListNode<>(arr[i]);
+            cur = cur.next;
+        }
+        cur.next = null;
+        return head;
+    }
+    public static void main(String[] args){
+        Learn learn = new Learn();
+        Integer[] arr = {1,2,3};
+        LearnListNode<Integer>head = learn.createLinkedList(arr);
+        LearnListNode<Integer> cur = head;
+        if (head != null) {
+            for(;cur != null ;cur = cur.next){
+                System.out.println(cur.val);
+            }
+        }
     }
 }
---
+++ src/practice/LearnListNode.java
@@ -0,0 +1,7 @@
+package practice;
+
+public class LearnListNode<E>{
+    E val;
+    LearnListNode<E> next;
+    public LearnListNode(E val){this.val = val;}
+}
