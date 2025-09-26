7a087105f85282eb0e98a2e59bda019bd4f04f63
lyh <liu1564684361@163.com>
2025-09-26 23:08:44 +0800

    A day without coding

---
+++ src/Leave/DayWithoutCoding25926.java
@@ -0,0 +1,9 @@
+package Leave;
+
+public class DayWithoutCoding25926 {
+//    Today I am too lazy,
+//    I have chatted with mangy friends and my mother,
+//    it makes me very happy,
+//    that I am lazy to da other things
+//    I apologize for it
+}
