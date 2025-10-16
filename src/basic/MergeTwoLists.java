23ea55f65d40c249c63cb570fafddb41bcf543ab
lyh <liu1564684361@163.com>
2025-10-16 17:18:25 +0800

    Second version MergeTwoLists

--- src/basic/MergeTwoLists.java
+++ src/basic/MergeTwoLists.java
@@ -4,14 +4,24 @@ import basic.ReverseList.ListNode;
 
 public class MergeTwoLists {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2){
-        if(list1 == null) return list2;
-        if(list2 == null) return list1;
-        if(list1.val < list2.val) {
-            list1.next = mergeTwoLists(list1.next,list2);
-            return list1;
-        }else{
-            list2.next = mergeTwoLists(list1,list2.next);
-            return list2;
+        ListNode dummyNode = new ListNode(-1);
+        ListNode cur = dummyNode;
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val){
+                cur.next = list1;
+                list1 = list1.next;
+            }else{
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
         }
+        if(list1 != null){
+            cur.next = list1;
+        }else{
+            cur.next = list2;
+        }
+
+        return dummyNode.next;
     }
 }
