f4f2821cf276853897e50904c0dcd7052b4ab936
lyh <liu1564684361@163.com>
2025-10-17 17:56:02 +0800

    First version Insert and HashCycle

---
+++ src/basic/HashCycle.java
@@ -0,0 +1,16 @@
+package basic;
+
+import basic.ReverseList.ListNode;
+
+public class HashCycle {
+    public boolean hasCycle(ListNode head){
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow) return true;
+        }
+        return false;
+    }
+}
